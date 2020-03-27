a1d621288ce6019cb8375657218a1e8d903d53f3
Amirparsa <Amirparsa.s@aut.ac.ir>
2020-03-28 03:57:51 +0430

    Add Disk class

---
+++ src/com/amirparsa/salmankhah/Disk.java
@@ -0,0 +1,29 @@
+package com.amirparsa.salmankhah;
+
+/**
+ * Represents a Disk in game
+ */
+public class Disk {
+    //Sign of the disk
+    char sign;
+    //Position of the disk
+    Point position;
+
+    public Disk(){
+        this('\0',0,0);
+    }
+
+    public Disk(char sign, int x, int y){
+        this.sign = sign;
+        position = new Point(x,y);
+    }
+
+    public void setSign(char sign){
+        this.sign = sign;
+    }
+
+    public void setPosition(int x,int y){
+        position.setX(x);
+        position.setY(y);
+    }
+}
