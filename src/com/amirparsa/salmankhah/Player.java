8c7d42045472f33c889d2eed520243fcddb021b6
Amirparsa <Amirparsa.s@aut.ac.ir>
2020-03-28 16:38:26 +0430

    add placeDisk method - change fields to private

--- src/com/amirparsa/salmankhah/Player.java
+++ src/com/amirparsa/salmankhah/Player.java
@@ -3,9 +3,9 @@ package com.amirparsa.salmankhah;
 import java.util.ArrayList;
 
 class Player {
-    String name;
-    char sign;
-    Board board;
+    private String name;
+    private char sign;
+    private Board board;
 
     public Player(String name, char sign) {
         this.name = name;
@@ -28,8 +28,14 @@ class Player {
     public void setName(String name) {
         this.name = name;
     }
+    public void placeDisk(Point position){
+        board.getDisk(position.getX(),position.getY()).setSign(sign);
+    }
 
-    private ArrayList<Point> availableMoves() {
+    public void setBoard(Board board){
+        this.board = board;
+    }
+    public ArrayList<Point> availableMoves() {
         ArrayList<Disk> playerDisks = getDisks();
         ArrayList<Disk> opponentDisks = getOpponentDisks();
         ArrayList<Point> availableDisks = new ArrayList<>();
@@ -38,17 +44,15 @@ class Player {
                 Point playerPosition = playerDisk.getPosition();
                 Point opponentPosition = opponentDisk.getPosition();
                 if (playerPosition.isNeighbor(opponentPosition)) {
-                    Point reflectPoint= playerPosition.reflection(opponentPosition);
-                    Disk reflectDisk = board.getDisk(reflectPoint.getX(),reflectPoint.getY());
-                    if (reflectPoint.isValid() && reflectDisk.getSign()=='\0')
+                    Point reflectPoint = playerPosition.reflection(opponentPosition);
+                    Disk reflectDisk = board.getDisk(reflectPoint.getX(), reflectPoint.getY());
+                    if (reflectPoint.isValid() && reflectDisk.getSign() == '\0')
                         availableDisks.add(reflectPoint);
-
                 }
             }
         }
         return availableDisks;
     }
-    //public void placeDisk(Point position){}
 
     private ArrayList<Disk> getDisks() {
         ArrayList<Disk> disks = new ArrayList<>();
@@ -68,11 +72,11 @@ class Player {
         return disks;
     }
 
-    public void showAvailableMoves(){
-        int index=1;
+    public void showAvailableMoves() {
+        int index = 1;
         ArrayList<Point> points = availableMoves();
-        System.out.println("Available Moves:");
-        for(Point point : points) {
+        System.out.println("available moves:");
+        for (Point point : points) {
             System.out.print(index + ") ");
             point.print();
             index++;
